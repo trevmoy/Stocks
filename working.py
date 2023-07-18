5d3d9bf83b34f8c71a85ff69174589532b3dc39c
Trevor Moy <51927676+trevmoy@users.noreply.github.com>
2023-07-18 13:42:33 -0400

    Tweaks and improvements
    Added titles to the columns. Modified the code so that the tickers and live prices would not overlap with the prices.

--- working.py
+++ working.py
@@ -11,18 +11,18 @@ def main():
     wb = load_workbook('Stocks.xlsx')
     ws = wb.active
 
-    # Indexing through the rows and columns to make modifications
+    # Adds titles to columns
+    ws['A' + str(1)] = "Ticker"
+    ws['B' + str(1)] = "Live Price"
 
+    # Indexing through the rows and columns to make modifications
     nasdaq: list = stocks.get_ticker()
-    for row in range(1, 20):
-        char = 'A'
-        ws[char + str(row)] = str(nasdaq[row])
-        char = 'B'
-        ws[char + str(row)] =  float("{:.2f}".format(stocks.get_price(nasdaq[row])))
+    for row in range(2, 21):
+        ws['A' + str(row)] = str(nasdaq[row-1])
+        ws['B' + str(row)] =  float("{:.2f}".format(stocks.get_price(nasdaq[row-1])))
 
             
 
-    # char +str(row)
     # Saves the document at the end of the code to update the actual excel sheet
     wb.save('Stocks.xlsx')
 
